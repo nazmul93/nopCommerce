1541ed34e395132d5398d091f7ecbebb89edfe50
Nazmul Hasan <nazmulhasan200@google.com>
2020-06-25 20:56:52 +0600

    Added Factory Class in Admin Area

--- Presentation/Nop.Web/Areas/Admin/Controllers/CompanyController.cs
+++ Presentation/Nop.Web/Areas/Admin/Controllers/CompanyController.cs
@@ -1,13 +1,98 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Nop.Core;
+using Nop.Core.Domain.Catalog;
+using Nop.Core.Domain.Discounts;
+using Nop.Services.Catalog;
+using Nop.Services.Customers;
+using Nop.Services.Discounts;
+using Nop.Services.ExportImport;
+using Nop.Services.Localization;
+using Nop.Services.Logging;
+using Nop.Services.Messages;
+using Nop.Services.Media;
+using Nop.Services.Security;
+using Nop.Services.Seo;
+using Nop.Services.Stores;
+using Nop.Web.Areas.Admin.Factories;
+using Nop.Web.Areas.Admin.Infrastructure.Mapper.Extensions;
+using Nop.Web.Areas.Admin.Models.Catalog;
+using Nop.Web.Framework.Controllers;
+using Nop.Web.Framework.Mvc;
+using Nop.Web.Framework.Mvc.Filters;
 
 namespace Nop.Web.Areas.Admin.Controllers
 {
     public class CompanyController : Controller
     {
+        #region Fields
+
+        private readonly IAclService _aclService;
+        private readonly ICategoryModelFactory _categoryModelFactory;
+        private readonly ICategoryService _categoryService;
+        private readonly ICustomerActivityService _customerActivityService;
+        private readonly ICustomerService _customerService;
+        private readonly IDiscountService _discountService;
+        private readonly IExportManager _exportManager;
+        private readonly IImportManager _importManager;
+        private readonly ILocalizationService _localizationService;
+        private readonly ILocalizedEntityService _localizedEntityService;
+        private readonly INotificationService _notificationService;
+        private readonly IPermissionService _permissionService;
+        private readonly IPictureService _pictureService;
+        private readonly IProductService _productService;
+        private readonly IStoreMappingService _storeMappingService;
+        private readonly IStoreService _storeService;
+        private readonly IUrlRecordService _urlRecordService;
+        private readonly IWorkContext _workContext;
+
+        #endregion
+
+        #region Ctor
+
+        public CompanyController(IAclService aclService,
+            ICategoryModelFactory categoryModelFactory,
+            ICategoryService categoryService,
+            ICustomerActivityService customerActivityService,
+            ICustomerService customerService,
+            IDiscountService discountService,
+            IExportManager exportManager,
+            IImportManager importManager,
+            ILocalizationService localizationService,
+            ILocalizedEntityService localizedEntityService,
+            INotificationService notificationService,
+            IPermissionService permissionService,
+            IPictureService pictureService,
+            IProductService productService,
+            IStoreMappingService storeMappingService,
+            IStoreService storeService,
+            IUrlRecordService urlRecordService,
+            IWorkContext workContext)
+        {
+            _aclService = aclService;
+            _categoryModelFactory = categoryModelFactory;
+            _categoryService = categoryService;
+            _customerActivityService = customerActivityService;
+            _customerService = customerService;
+            _discountService = discountService;
+            _exportManager = exportManager;
+            _importManager = importManager;
+            _localizationService = localizationService;
+            _localizedEntityService = localizedEntityService;
+            _notificationService = notificationService;
+            _permissionService = permissionService;
+            _pictureService = pictureService;
+            _productService = productService;
+            _storeMappingService = storeMappingService;
+            _storeService = storeService;
+            _urlRecordService = urlRecordService;
+            _workContext = workContext;
+        }
+
+        #endregion
         public IActionResult Index()
         {
             return View();
---
+++ Presentation/Nop.Web/Areas/Admin/Factories/ICompanyModelFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nop.Web.Areas.Admin.Factories
+{
+    public partial interface ICompanyModelFactory
+    {
+    }
+}
