7b2ab07551642e7b411f2470dac8f0752cc56b18
Nazmul Hasan <nazmulhasan200@google.com>
2020-06-25 21:02:30 +0600

    Added Company Search Model

---
+++ Presentation/Nop.Web/Areas/Admin/Models/Catalog/CompanySearchModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Nop.Web.Framework.Models;
+using Nop.Web.Framework.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nop.Web.Areas.Admin.Models.Catalog
+{
+    public class CompanySearchModel : BaseSearchModel
+    {
+        #region Ctor
+
+        public CompanySearchModel()
+        {
+            AvailableCompanies = new List<SelectListItem>();
+        }
+
+        #endregion
+
+        #region Properties
+
+        [NopResourceDisplayName("Admin.Catalog.Companies.List.SearchCompanyName")]
+        public string SearchCompanyName { get; set; }
+
+        public IList<SelectListItem> AvailableCompanies { get; set; }
+
+        #endregion
+    }
+}
