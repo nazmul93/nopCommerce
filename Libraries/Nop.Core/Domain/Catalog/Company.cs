ed9dd93f4b8e47f8cfadcd8c030b0cb264002b7d
Nazmul Hasan <nazmulhasan200@google.com>
2020-06-25 17:50:32 +0600

    Added Company Class in Nop.Core

---
+++ Libraries/Nop.Core/Domain/Catalog/Company.cs
@@ -0,0 +1,42 @@
+﻿using Nop.Core.Domain.Localization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nop.Core.Domain.Catalog
+{
+    public class Company : BaseEntity, ILocalizedEntity
+    {
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the picture identifier
+        /// </summary>
+        public int PictureId { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the entity is published
+        /// </summary>
+        public bool Published { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the entity has been deleted
+        /// </summary>
+        public bool Deleted { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time of instance creation
+        /// </summary>
+        public DateTime CreatedOnUtc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time of instance update
+        /// </summary>
+        public DateTime UpdatedOnUtc { get; set; }
+    }
+}
