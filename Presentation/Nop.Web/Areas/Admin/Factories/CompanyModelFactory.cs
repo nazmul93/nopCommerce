5ad6d9367fc995b25fafba03a9f04a703f204380
Nazmul Hasan <nazmulhasan200@google.com>
2020-06-25 21:36:16 +0600

    Added Factory Model class in Admin Area

---
+++ Presentation/Nop.Web/Areas/Admin/Factories/CompanyModelFactory.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nop.Core.Domain.Catalog;
+using Nop.Core.Domain.Discounts;
+using Nop.Services.Catalog;
+using Nop.Services.Discounts;
+using Nop.Services.Localization;
+using Nop.Services.Seo;
+using Nop.Web.Areas.Admin.Infrastructure.Mapper.Extensions;
+using Nop.Web.Areas.Admin.Models.Catalog;
+using Nop.Web.Framework.Extensions;
+using Nop.Web.Framework.Factories;
+using Nop.Web.Framework.Models.Extensions;
+
+namespace Nop.Web.Areas.Admin.Factories
+{
+    public partial class CompanyModelFactory : ICompanyModelFactory
+    {
+        #region Fields
+
+        private readonly CatalogSettings _catalogSettings;
+        private readonly IAclSupportedModelFactory _aclSupportedModelFactory;
+        private readonly IBaseAdminModelFactory _baseAdminModelFactory;
+        private readonly ICompanyService _companyService;
+        private readonly IDiscountService _discountService;
+        private readonly IDiscountSupportedModelFactory _discountSupportedModelFactory;
+        private readonly ILocalizationService _localizationService;
+        private readonly ILocalizedModelFactory _localizedModelFactory;
+        private readonly IProductService _productService;
+        private readonly IStoreMappingSupportedModelFactory _storeMappingSupportedModelFactory;
+        private readonly IUrlRecordService _urlRecordService;
+
+        #endregion
+
+        #region Ctor
+
+        public CompanyModelFactory(CatalogSettings catalogSettings,
+            IAclSupportedModelFactory aclSupportedModelFactory,
+            IBaseAdminModelFactory baseAdminModelFactory,
+            ICompanyService companyService,
+            IDiscountService discountService,
+            IDiscountSupportedModelFactory discountSupportedModelFactory,
+            ILocalizationService localizationService,
+            ILocalizedModelFactory localizedModelFactory,
+            IProductService productService,
+            IStoreMappingSupportedModelFactory storeMappingSupportedModelFactory,
+            IUrlRecordService urlRecordService)
+        {
+            _catalogSettings = catalogSettings;
+            _aclSupportedModelFactory = aclSupportedModelFactory;
+            _baseAdminModelFactory = baseAdminModelFactory;
+            _companyService = companyService;
+            _discountService = discountService;
+            _discountSupportedModelFactory = discountSupportedModelFactory;
+            _localizationService = localizationService;
+            _localizedModelFactory = localizedModelFactory;
+            _productService = productService;
+            _storeMappingSupportedModelFactory = storeMappingSupportedModelFactory;
+            _urlRecordService = urlRecordService;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Prepare category search model
+        /// </summary>
+        /// <param name="searchModel">Category search model</param>
+        /// <returns>Category search model</returns>
+        public virtual CompanySearchModel PrepareCompanySearchModel(CompanySearchModel searchModel)
+        {
+            if (searchModel == null)
+                throw new ArgumentNullException(nameof(searchModel));
+
+            //prepare available stores
+            _baseAdminModelFactory.PrepareStores(searchModel.AvailableCompanies);
+
+            //searchModel.HideStoresList = _catalogSettings.IgnoreStoreLimitations || searchModel.AvailableCompanies.SelectionIsNotPossible();
+
+            //prepare page parameters
+            searchModel.SetGridPageSize();
+
+            return searchModel;
+        }
+
+        /// <summary>
+        /// Prepare paged category list model
+        /// </summary>
+        /// <param name="searchModel">Category search model</param>
+        /// <returns>Category list model</returns>
+        public virtual CompanyListModel PrepareCompanyListModel(CompanySearchModel searchModel)
+        {
+            if (searchModel == null)
+                throw new ArgumentNullException(nameof(searchModel));
+
+            //get companies
+            var companies = _companyService.GetAllCompanies(companyName: searchModel.SearchCompanyName,
+                showHidden: true,
+                pageIndex: searchModel.Page - 1, pageSize: searchModel.PageSize);
+
+            //prepare grid model
+            var model = new CompanyListModel().PrepareToGrid(searchModel, companies, () =>
+            {
+                return companies.Select(company =>
+                {
+                    //fill in model values from the entity
+                    var companyModel = company.ToModel<CompanyModel>();
+
+                    //fill in additional values (not existing in the entity)
+                    //categoryModel.Breadcrumb = _companyService.GetFormattedBreadCrumb(company);
+                    //categoryModel.SeName = _urlRecordService.GetSeName(company, 0, true, false);
+
+                    return companyModel;
+                });
+            });
+
+            return model;
+        }
+
+        /// <summary>
+        /// Prepare category model
+        /// </summary>
+        /// <param name="model">Category model</param>
+        /// <param name="category">Category</param>
+        /// <param name="excludeProperties">Whether to exclude populating of some properties of model</param>
+        /// <returns>Category model</returns>
+        public virtual CompanyModel PrepareCompanyModel(CompanyModel model, Company company
+            , bool excludeProperties = false)
+        {
+            Action<CategoryLocalizedModel, int> localizedModelConfiguration = null;
+
+            if (company != null)
+            {
+                //fill in model values from the entity
+                if (model == null)
+                {
+                    model = company.ToModel<CompanyModel>();
+                    //model.SeName = _urlRecordService.GetSeName(category, 0, true, false);
+                }
+            }
+
+            //set default values for the new model
+            if (company == null)
+            {
+                model.PageSize = _catalogSettings.DefaultCategoryPageSize;
+                //model.PageSizeOptions = _catalogSettings.DefaultCategoryPageSizeOptions;
+                model.Published = true;
+            }
+
+            //prepare localized models
+            if (!excludeProperties)
+                model.Locales = _localizedModelFactory.PrepareLocalizedModels(localizedModelConfiguration);
+
+            return model;
+        }
+
+        #endregion
+    }
+}
--- Presentation/Nop.Web/Areas/Admin/Factories/ICompanyModelFactory.cs
+++ Presentation/Nop.Web/Areas/Admin/Factories/ICompanyModelFactory.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Nop.Core.Domain.Catalog;
+using Nop.Web.Areas.Admin.Models.Catalog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,12 @@ namespace Nop.Web.Areas.Admin.Factories
 {
     public partial interface ICompanyModelFactory
     {
+        CompanySearchModel PrepareCompanySearchModel(CompanySearchModel searchModel);
+
+        CompanyListModel PrepareCompanyListModel(CompanySearchModel searchModel);
+
+        CompanyModel PrepareCompanyModel(CompanyModel model, Company company, 
+            bool excludeProperties = false);
+
     }
 }
